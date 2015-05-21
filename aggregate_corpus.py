eed39af8dd3ee31ee2d6328a21bb02e348cc75c5
Bernhard Haslhofer <bernhard.haslhofer@ait.ac.at>
2015-05-21 13:29:55 +0200

    generation successful

--- aggregate_corpus.py
+++ aggregate_corpus.py
@@ -18,182 +18,11 @@ from bs4.element import Tag, NavigableString
 import requests
 
 
-WIKI_INSTANCE = 'http://en.wikipedia.org'
-
-
-def parse_concepts(skosfile):
-    """Retuns a list of {pptid, dbpedia_uri} dictionaries."""
-    r = re.compile(('<(.*)> '
-                    '<http:\/\/www\.w3\.org\/2004\/02\/skos\/core#exactMatch> '
-                    '<(.*)>'))
-    concepts = []
-    with open(skosfile, 'r') as f:
-        for line in f:
-            match = re.match(r, line)
-            if match:
-                s = match.group(1)
-                o = match.group(2)
-                pptid = os.path.basename(s)
-                concepts.append({'pptid': pptid, 'dbpedia_uri': o})
-    return concepts
-
-
-def retrieve_wikipageid(dbpedia_uri):
-    dbpedia_uri_json = dbpedia_uri.replace("resource", "data")
-    dbpedia_uri_json += ".json"
-    r = requests.get(dbpedia_uri_json)
-    if(r.status_code != 200):
-        print("FAILURE. Request", r.url, "failed.")
-        return None
-    else:
-        result = r.json()
-        resource = result.get(dbpedia_uri)
-        if not resource:
-            return None
-        page_id = resource.get("http://dbpedia.org/ontology/wikiPageID")
-        if not page_id:
-            return None
-        return page_id[0]['value']
-
-
-def retrieve_wikipedia_page(page_id):
-    API_URL = WIKI_INSTANCE + '/w/api.php'
-    USER_AGENT = 'wikicorpus (https://github.com/behas/wikigrouth/)'
-    params = {
-        'action': 'query',
-        'pageids': page_id,
-        'prop': 'revisions',
-        'rvlimit': 1,
-        'rvprop': 'content',
-        'rvparse': False,
-        'redirects': True,
-        'format': 'json'
-    }
-    headers = {
-        'User-Agent': USER_AGENT
-    }
-    r = requests.get(API_URL, params=params, headers=headers)
-    if(r.status_code != 200):
-        print("FAILURE. Request", r.url, "failed.")
-        return None
-    response = r.json()
-    pages = response['query']['pages']
-    page = next(iter(pages.values()))
-    page_html = page['revisions'][0]['*']
-    return page_html
-
-
-def clean_wiki_page(soup):
-    """
-    Removes non-textual parts of page. Taken from:
-    https://git.wikimedia.org/blob/mediawiki%2Fextensions%2FTextExtracts/46f6a992fa84f308f4990b6d4bdc337c07868d8c/TextExtracts.php
-    """
-    for tag in soup.find_all(['table', 'div', 'ul', 'li', 'tr', 'th']):
-        tag.decompose()
-    for tag in soup.find_all(class_=["mw-editsection",
-                                     "error", "nomobile",
-                                     "noprint", "noexcerpt"]):
-        tag.decompose()
-    for tag in soup.find_all('sup', 'reference'):
-        tag.decompose()
-    return soup
-
-
-def extract_ne_mentions(wiki_page_html):
-    """Extracts raw text and named entities from Wikipedia HTML page"""
-    soup = BeautifulSoup(wiki_page_html)
-
-    content = ""
-    named_entities = []
-    offset = 0
-
-    for tag in soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5']):
-        contents = tag.contents
-        if(contents is None or len(contents) == 0):
-            continue
-        # handle paragraphs
-        if tag.name == 'p':
-            for element in contents:
-                text = ""
-                if(type(element) is NavigableString):
-                    text = str(element)
-                if(type(element) is Tag):
-                    text += element.get_text()
-                    if(element.name == 'a' and 'href' in element.attrs):
-                        link = WIKI_INSTANCE + element['href']
-                        ne = {'offset': offset, 'text': text, 'link': link}
-                        named_entities.append(ne)
-                content += text
-                offset += len(text)
-            content += '\n'
-            offset += 1
-        if tag.name.startswith('h'):
-            level = int(tag.name[1])
-            h = '=' * level + ' ' + tag.get_text() + ' ' + '=' * level + '\n\n'
-            content += h
-            offset += len(h)
-    return (content, named_entities)
-
-
-def aggregate_corpus_old(skosfile, override=False):
-    """Main corpus aggregation workflow function."""
-    print("Parsing DBPedia concepts from", skosfile)
-    concepts = parse_concepts(skosfile)
-    if len(concepts) == 0:
-        print("No concepts found. Aborting.")
-        return
-    outputpath = skosfile[:-3]
-    # Preparations
-    print("Preparing output path", outputpath)
-    if not os.path.exists(outputpath):
-        os.makedirs(outputpath)
-    index_csv = outputpath + ".csv"
-    ne_csv = outputpath + "_entities.csv"
-    # Running aggregation procedure
-    with open(index_csv, 'w') as i_csv, open(ne_csv, 'w') as ne_csv:
-        # preparing CSV writers
-        i_fields = ['pptid', 'dbpedia_uri', 'txt_file', 'html_file']
-        i_writer = csv.DictWriter(i_csv, fieldnames=i_fields)
-        i_writer.writeheader()
-        ne_fields = ['doc_id', 'offset', 'text', 'link']
-        ne_writer = csv.DictWriter(ne_csv, fieldnames=ne_fields)
-        ne_writer.writeheader()
-        for concept in concepts:
-            print("\nProcessing concept", concept['pptid'], "...")
-            outputpath_text = outputpath + '/txt'
-            if not os.path.exists(outputpath_text):
-                os.makedirs(outputpath_text)
-            outputpath_html = outputpath + '/html'
-            if not os.path.exists(outputpath_html):
-                os.makedirs(outputpath_html)
-            text_file = outputpath_text + '/' + concept['pptid'] + ".txt"
-            html_file = outputpath_html + '/' + concept['pptid'] + ".html"
-            if os.path.exists(text_file) or os.path.exists(html_file):
-                if not override:
-                    print("Files already aggregated")
-                    continue
-            print("Retrieving wikiPageID from", concept['dbpedia_uri'], "...")
-            wikiPageID = retrieve_wikipageid(concept['dbpedia_uri'])
-            print("Retrieving Wikipedia page", wikiPageID, "...")
-            page_html = retrieve_wikipedia_page(wikiPageID)
-            print("Extracting raw text and named entities...")
-            content, named_entities = extract_ne_mentions(page_html)
-            print("Found", len(named_entities), "named entity mentions.")
-            # writing html and txt content
-            with open(text_file, "w") as out_file:
-                out_file.write(content)
-                concept['txt_file'] = os.path.basename(text_file)
-            with open(html_file, "w") as out_file:
-                out_file.write(page_html)
-                concept['html_file'] = os.path.basename(html_file)
-            i_writer.writerow(concept)
-            # writing extracted named entities
-            for entity in named_entities:
-                entity['doc_id'] = concept['pptid'] + ".txt"
-                ne_writer.writerow(entity)
-
-
 class Wikipage:
+
+    # TODO: remove DBPedia dependency and use Wikipedia export page instead
+
+    WIKI_INSTANCE = 'http://en.wikipedia.org'
 
     def __init__(self, uri, html_file=None):
         if(html_file is not None):
@@ -205,6 +34,7 @@ class Wikipage:
     def _process_page(self, page_html):
         """Extracts raw text and named entities from Wikipedia HTML page"""
         soup = BeautifulSoup(page_html)
+        soup = self.clean_wiki_page(soup)
 
         content = ""
         entities = []
@@ -223,8 +53,8 @@ class Wikipage:
                     if(type(element) is Tag):
                         text += element.get_text()
                         if(element.name == 'a' and 'href' in element.attrs):
-                            link = WIKI_INSTANCE + element['href']
-                            ne = {'offset': offset, 'text': text, 'link': link}
+                            uri = Wikipage.WIKI_INSTANCE + element['href']
+                            ne = {'offset': offset, 'text': text, 'uri': uri}
                             entities.append(ne)
                     content += text
                     offset += len(text)
@@ -237,6 +67,18 @@ class Wikipage:
                 content += h
                 offset += len(h)
         return (content, entities)
+
+    def clean_wiki_page(self, soup):
+        """Removes non-textual parts of page."""
+        for tag in soup.find_all(['table', 'div', 'ul', 'li', 'tr', 'th']):
+            tag.decompose()
+        for tag in soup.find_all(class_=["mw-editsection",
+                                         "error", "nomobile",
+                                         "noprint", "noexcerpt"]):
+            tag.decompose()
+        for tag in soup.find_all('sup', 'reference'):
+            tag.decompose()
+        return soup
 
     def _load_from_file(self, html_file):
         print("Loading page from cache", html_file)
@@ -273,7 +115,7 @@ class Wikipage:
 
     def _retrieve_wikipedia_page(self, wikipageID):
         print("Retrieving Wikipedia page", wikipageID, "...")
-        API_URL = WIKI_INSTANCE + '/w/api.php'
+        API_URL = Wikipage.WIKI_INSTANCE + '/w/api.php'
         USER_AGENT = 'wikicorpus (https://github.com/behas/wikigrouth/)'
         params = {
             'action': 'query',
@@ -358,17 +200,50 @@ class Corpus:
         with open(filename, 'w') as f:
             f.write(content)
 
+    def to_dbpedia(self, wikipedia_uri):
+        return wikipedia_uri.replace("en.wikipedia.org/wiki",
+                                     "dbpedia.org/resource")
+
     def aggregate(self, override=False):
         self._ensurepaths()
-        for uri in self.uris:
-            print("\nProcessing uri", uri, "...")
-            if(os.path.exists(self.htmlfile(uri)) and not override):
-                page = Wikipage(uri, self.htmlfile(uri))
-            else:
-                page = Wikipage(uri)
-            self._write_file(self.htmlfile(uri), page.html, override)
-            self._write_file(self.textfile(uri), page.text, override)
-            print(page.entities)
+        fieldnames_index = ['doc_id', 'uri', 'html_file', 'text_file']
+        fieldnames_entity = ['doc_id', 'offset', 'text', 'uri', 'in_seed']
+        with open(self.indexfile, 'w') as index_csv, \
+                open(self.entityfile, 'w') as entity_csv:
+
+            index_writer = csv.DictWriter(index_csv,
+                                          fieldnames=fieldnames_index)
+            index_writer.writeheader()
+            entity_writer = csv.DictWriter(entity_csv,
+                                           fieldnames=fieldnames_entity)
+            entity_writer.writeheader()
+
+            for index, uri in enumerate(self.uris):
+                print("\nProcessing uri", uri, "...")
+                if(os.path.exists(self.htmlfile(uri)) and not override):
+                    page = Wikipage(uri, self.htmlfile(uri))
+                else:
+                    page = Wikipage(uri)
+                self._write_file(self.htmlfile(uri), page.html, override)
+                self._write_file(self.textfile(uri), page.text, True)
+                # Recording output in CSV files
+                html_file_rel = os.path.basename(self.htmlfile(uri))
+                text_file_rel = os.path.basename(self.textfile(uri))
+                index_writer.writerow({'doc_id': index,
+                                       'uri': uri,
+                                       'html_file': html_file_rel,
+                                       'text_file': text_file_rel})
+                for entity in page.entities:
+                    entity_uri = self.to_dbpedia(entity['uri'])
+                    if(entity_uri in self.uris):
+                        in_seed = 1
+                    else:
+                        in_seed = 0
+                    entity_writer.writerow({'doc_id': index,
+                                            'offset': entity['offset'],
+                                            'text': entity['text'],
+                                            'uri': entity_uri,
+                                            'in_seed': in_seed})
 
 
 def aggregate_corpus(seedfile, override=False):
